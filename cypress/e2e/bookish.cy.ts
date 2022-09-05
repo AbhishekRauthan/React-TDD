cbe5c932404ec26282d786d3dc8c37c7df587e17
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-05 15:25:05 +0530

    The following was done:
    	- changed title in index.html.
    	- in App, created an array to map books.
    	- in bookish, created test for that.

--- cypress/e2e/bookish.cy.ts
+++ cypress/e2e/bookish.cy.ts
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(2);
+
+      const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
+    });
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Typography } from "@mui/material";
 
-export default App
+const App = () => {
+  const books = [
+    {
+      name: "Refactoring",
+    },
+    {
+      name: "Domain-driven design",
+    },
+  ];
+  return (
+    <>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+      <div className="" data-test="book-list">
+        {books.map((book) => (
+          <div className="book-item">
+            <h2 className="title">{book.name}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
