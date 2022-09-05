dafbd17e55d610b4fc44f0f88336928f8d0c8451
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-05 19:31:40 +0530

    The following was done:
    	- impleamented fetching books data and setting it to books state.
    	- impleamented before and before each mocha hooks.
    	- used cy.request to make url requests

--- cypress/e2e/bookish.cy.ts
+++ cypress/e2e/bookish.cy.ts
@@ -1,12 +1,30 @@
 describe("empty spec", () => {
+  before(() => {
+    cy.request("DELETE", "http://localhost:8080/books?_cleanup=true");
+  });
+  beforeEach(() => {
+    const books = [
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-driven design", id: 2 },
+      { name: "Building Microservices", id: 3 },
+    ];
+
+    books.map((book) =>
+      cy.request("POST", "http://localhost:8080/books", book)
+    );
+  });
   it("passes", () => {
     cy.visit("http://localhost:5173/");
     cy.get('div[data-test="book-list"]').should("exist");
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(2);
+      expect(books).to.have.length(3);
 
       const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
-      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
+      expect(titles).to.deep.equal([
+        "Refactoring",
+        "Domain-driven design",
+        "Building Microservices",
+      ]);
     });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 const App = () => {
-  const books = [
-    {
-      name: "Refactoring",
-    },
-    {
-      name: "Domain-driven design",
-    },
-  ];
+  const [books, setBooks] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get("http://localhost:8080/books");
+      setBooks(res.data);
+    };
+    fetchBooks();
+  }, []);
+
   return (
     <>
       <Typography variant="h2" component="h2" data-test="heading">
         Bookish
       </Typography>
-      <BookList books={books}/>
+      <BookList books={books} />
     </>
   );
 };
