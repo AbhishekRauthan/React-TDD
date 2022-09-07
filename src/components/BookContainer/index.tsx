dcfa52c83d5ab6fd004d0d110ef905c08c8f8ec4
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-07 13:08:19 +0530

    Revert "The following was done:"
    This reverts commit 2ac7bbcadcd6ac5bec5e74570a4d8051834e6410.

--- src/components/BookContainer/index.tsx
+++
@@ -1,9 +0,0 @@
-import getData from "../../hooks/getData";
-import BookList from "../BookList";
-
-const BooklistContainer = () => {
-  const {books,error,loading} = getData("http://localhost:8080/books")
-  return <BookList books={books} error={error} loading={loading} />;
-};
-
-export default BooklistContainer;
