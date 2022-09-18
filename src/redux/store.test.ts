93d6b15f160efebc6f3b05c4489780b0023dacd3
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-18 14:33:55 +0530

    The following was done:
    	- renamed reducer.ts to bookSlice.ts and updated all import statements of reducer in other files.
    	- added a test to see if result of fetchBooks rejected is truthy and refactored it a bit.
    	- added export statements for types.

--- src/redux/store.test.ts
+++ src/redux/store.test.ts
@@ -1,28 +1,31 @@
 import { describe, expect, it, vi } from "vitest";
 import store from "./store";
 import axios from "axios";
-import reducer, { fetchBooks } from "./reducer";
+import reducer, { fetchBooks } from "./bookSlice";
 
 describe("Testing Store", () => {
-  it("Fetches book from remote", () => {
+  const state = {
+    loading: false,
+    error: false,
+    books: [],
+  };
+  it("Fetches book from remote", async () => {
     const books = [{ id: 1, name: "Refactoring" }];
     axios.get = vi
       .fn()
-      .mockImplementation(() => Promise.resolve({ data: books }));
+      .mockImplementation(() => Promise.resolve(books));
 
-    store.dispatch(fetchBooks("")).then(() => {
-      const state = store.getState().books;
-      expect(state.books.length).toEqual(1);
-      expect(state.books).toEqual(books);
-    });
+    await store.dispatch(fetchBooks(""));
+    const state = store.getState();
+    expect(state.books.length).toEqual(1);
+    expect(state.books).toEqual(books);
   });
-  it("returns loading to true", async () => {
-    const state = {
-      loading: false,
-      error: false,
-      books: [],
-    };
-    const result = reducer(state,{type: "books/fetchBooks/pending"});
+  it("returns loading to be true", async () => {
+    const result = reducer(state, { type: "books/fetchBooks/pending" });
     expect(result.loading).toBeTruthy();
+  });
+  it("returns error to be true", async () => {
+    const result = reducer(state, { type: "books/fetchBooks/rejected" });
+    expect(result.error).toBeTruthy();
   });
 });
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import bookReducer from "./reducer";
+import bookReducer from "./bookSlice";
 
 const store = configureStore({
-  reducer: {
-    books: bookReducer,
-  },
+  reducer: bookReducer,
 });
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
