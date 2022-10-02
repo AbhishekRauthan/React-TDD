e5ff0195c553f01d4ec8b33efc014833e9a43b24
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-10-02 18:44:40 +0530

    Wrapped ChakraProvider around App. changed #id element from div to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 import store from "./redux/store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <ChakraProvider>
+        <Router>
+          <App />
+        </Router>
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
